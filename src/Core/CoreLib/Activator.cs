da9d31a3c6bdecc7db3903c7eee503076b4281d4
kirkwoll <satoru@lindenlab.com>
2013-06-30 13:25:59 -0700

    Add overloads to allow you to pass arguments

--- src/Core/CoreLib/Activator.cs
+++ src/Core/CoreLib/Activator.cs
@@ -7,5 +7,21 @@ namespace System {
         public static object CreateInstance(Type type) {
             return null;
         } 
+
+        public static object CreateInstance(Type type, object arg1) {
+            return null;
+        } 
+
+        public static object CreateInstance(Type type, object arg1, object arg2) {
+            return null;
+        } 
+
+        public static object CreateInstance(Type type, object arg1, object arg2, object arg3) {
+            return null;
+        } 
+
+        public static object CreateInstance(Type type, object[] args) {
+            return null;
+        } 
     }
 }
